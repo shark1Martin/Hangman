37833b16f24e2f283704b4b8b305fa0e87a1d520
Martino <162070213+shark1Martin@users.noreply.github.com>
2024-06-25 09:59:04 -0400

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -5,21 +5,10 @@ import java.util.Scanner;
 public class Hangman {
 
 
-
-
-
-
     private static String theWord;
     private static char theCharacter;
     private static final ArrayList<Character> charChecker = new ArrayList<>();
     private static final ArrayList<Character> charFiller = new ArrayList<>();
-
-
-
-
-
-
-
 
 
     public static void Intro() {
@@ -27,15 +16,6 @@ public class Hangman {
         System.out.println("[[WELCOME TO HANGMAN]]");
     
     }
-
-
-
-
-
-
-
-
-
 
     public static void createWord() {
 
@@ -57,7 +37,7 @@ public class Hangman {
             for (int j = 0; j < theWord.length(); j++) {
                 charChecker.add(theWord.charAt(j));
             }
-            System.out.println(charChecker);
+            // System.out.println(charChecker);
 
             guessWord(theWord, charChecker);
         }
@@ -67,22 +47,6 @@ public class Hangman {
     public static void createPhrase() {
         // empty for now
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     public static void guessWord(String word, ArrayList<Character> character) {
@@ -96,120 +60,142 @@ public class Hangman {
         for (int l = 0; l < theWord.length(); l++) {
             charFiller.add(l, arrayFiller);
         }
-        System.out.println(charFiller);
+        // System.out.println(charFiller);
 
 
+        try (Scanner guess = new Scanner(System.in)) {
+            System.out.println("Player 2, you're up! ");
+
+            System.out.print("The word has " + word.length() + " letters: ");
+            for (int m = 0; m < word.length(); m++) {
+                System.out.print("_ ");
+            }
+
+            System.out.println("\n");
+            System.out.println("\nYou have 6 lives.");
+
+            char guessLetter = '~';
+            char[] letterList = word.toCharArray();
+            char[] guessedLetters = new char [6];
+            for (int k = 0; k < guessedLetters.length; k++) {
+                guessedLetters[k] = guessLetter;
+            }
 
 
+            boolean gameWinner = false;
+            int lives = 6;
+            int index = 0;
+            while (lives > 0) {
 
+                System.out.print("What character would you like to guess? ");
+                theCharacter = guess.next().charAt(0);
 
-        Scanner guess = new Scanner(System.in);
-        System.out.println("Player 2, you're up! ");
+                
+                // WHILE LOOP ATTEMPT [PART 1]
+                /*
+                 boolean theKey = true;
+                while (theKey) {
+                    for (int j = 0; j < 6; j++) {
+                        if (theCharacter != guessedLetters[j]) {
 
-        System.out.print("The word has " + word.length() + " letters: ");
-        for (int m = 0; m < word.length(); m++) {
-            System.out.print("_ ");
-        }
-
-        System.out.println("\n");
-
-
-        System.out.println("\nYou have 6 lives.");
-
-        char[] letterList = word.toCharArray();
-
-        boolean gameWinner = false;
-        int lives = 6;
-        while (lives > 0) {
-
-            System.out.print("What character would you like to guess? ");
-            theCharacter = guess.next().charAt(0);
-
-
-            
-            
-            /*
-            char target = theCharacter;
-            boolean whileKey = false;
-            while (!whileKey) {
-                for (int i = 0; i < charArray.length; i++ ) {
-                    if (charArray2[i] == target) {
-                        System.out.print("Guess a different letter. ");
-                        theCharacter = guess.next().charAt(0);
-                        target = theCharacter;
-                    } else {
-                        whileKey = true;
-                        break;
+                        } else if (theCharacter == guessedLetters[j]) {
+                            System.out.println("Try Again!");
+                            theCharacter = guess.next().charAt(0);
+                            break;
+                        } else if (j == 6) {
+                            theKey = false;
+                            break;
+                        }
                     }
                 }
-            } 
+                 */
+                
+                    
+                // WHILE LOOP ATTEMPT [PART 2]
+                /* 
+                System.out.println(letterList);
+                System.out.println(guessedLetters);
+                int counter9 = 0;
+                boolean key = true;
+                while (key) {
+                    System.out.print("What character would you like to guess? ");
+                    theCharacter = guess.next().charAt(0);
+                    for (int d = 0; d < word.length(); d++) {
+                        if (theCharacter == allChar[d]) {
+                            System.out.print("Guess again");
+                            theCharacter = guess.next().charAt(0);
+                            allChar[d] = theCharacter;
+                            key = false;
+                        }
+                    }
+                    allChar[counter9] = theCharacter;
+                }
+                counter9++;
                 */
-            
 
 
+                // [1] SETTING THE CHARACTER
+                int runThrough = 0;
+                while (runThrough < word.length()) {
+                    if (theCharacter == letterList[runThrough]) {
+                        charFiller.set(runThrough, theCharacter);
+                    }
+                    runThrough++;
+                }
 
-             int runThrough = 0;
-            while (runThrough < word.length()) {
-                if (theCharacter == letterList[runThrough]) {
-                    charFiller.set(runThrough, theCharacter);
-                } 
-                runThrough++;
-            }
-            System.out.println("\n" + charFiller);
+                
+                // [1] STAYS FIXED HERE
+                char[] charArray2 = new char[character.size()];
+                for (int x = 0; x < character.size(); x++) {
+                    charArray2[x] = charFiller.get(x);
+                }
+
+                
+                // [2] CALCULATING LIVES
+                int antiLivesCounter = 0;
+                for (char c : charArray) {
+                    if (theCharacter == c) {
+                        antiLivesCounter++;
+                    } 
+                }
+                if (antiLivesCounter == 0) {
+                    lives--;
+                    guessedLetters[index] = theCharacter;
+                    index++;
+                }       
+
+                
+                // [3] [DISPLAYING CONTENT]
+                System.out.println("\nYour guessed letters: " + Arrays.toString(guessedLetters));
+                System.out.println("\nThe word: " + charFiller);
+                System.out.println("\nYour lives: " + lives);
 
 
-
-            char[] charArray2 = new char[character.size()];
-            for (int x = 0; x < character.size(); x++) {
-                charArray2[x] = charFiller.get(x);
-            }
-
-
-
-            int antiLivesCounter = 0;
-            for (char c : charArray) {
-                if (theCharacter == c) {
-                    antiLivesCounter++;
+                // [4] CHECKING GAME WINNER
+                if (Arrays.equals(letterList, charArray2)) {
+                    gameWinner = true;
+                    break;
                 }
             }
-            if (antiLivesCounter == 0) {
-                lives--;
-            }          
-            System.out.println("\nYour lives: " + lives);
 
-
-            if (Arrays.equals(letterList, charArray2)) {
-                gameWinner = true;
-                break;
-            }
-        
+            gameResult(gameWinner, theWord);
         }
-
-        gameResult(gameWinner);
-
     }
 
 
-
-    public static void gameResult(boolean winner) {
+    public static void gameResult(boolean winner, String word) {
 
         if (winner) {
-            System.out.println("Player 2 has won!");
-            System.out.println("Easy word, innit?");
+            System.out.println("\nPlayer 2 has won!");
+            System.out.println("Easy word, innit?\n");
         } else {
-            System.out.println("Player 1 has won!");
-            System.out.println("impeccable word you have chosen");
+            System.out.println("\nThe word was " + word + "\n");
+            System.out.println("\nPlayer 1 has won!");
+            System.out.println("Impeccable word you have chosen.\n");
         }
-
     }
 
 
-
-
-
-
-
-    
     public static void main(String[] args) {
 
         // Intro
@@ -217,7 +203,6 @@ public class Hangman {
 
         // First Player
         createWord();
-
-
+        
     }
 }
